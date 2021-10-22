a4cee50d1e205890a2bcb830b9054a64652f60ef
Sanjay Parmar <info.sanjayparmar1987@gmail.com>
2021-10-22 11:24:39 +0530

    TODO Kaushik: Add new functions

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 async function getNearByPlaceSearch(key, location, radius, type) {
     let url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
     url += `?location=${location}&radius=${radius}&type=${type}&key=${key}`;
-    return await fetch(url, {
-        method: GET,
-        body: null,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
+async function getNearByPlaceTextSearch(key, location, radius, type) {
+    let url = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
+    url += `?location=${location}&radius=${radius}&query=${type}&key=${key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
+async function getLocationDetail(key, location) {
+    let url = 'https://maps.googleapis.com/maps/api/geocode/json';
+    url += `?address=${location}&key=${key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
+async function getDirectionRoutePath(key, origin, destination, waypoints) {
+    let url = 'https://maps.googleapis.com/maps/api/directions/json';
+    url += `?origin=${origin}&waypoints=${waypoints}&destination=${destination}&key=${key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 };
 
+
 module.exports = {
-    getNearByPlaceSearch
+    getNearByPlaceTextSearch,
+    getNearByPlaceSearch,
+    getLocationDetail,
+    getDirectionRoutePath
 }
