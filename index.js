c021d87d7bdf5d4ae2233e4b5e79bded786d3ef3
Sanjay Parmar <info.sanjayparmar1987@gmail.com>
2021-10-22 10:26:20 +0530

    TODO Kaushik:Add init

---
+++ index.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+async function getNearByPlaceSearch(key, location, radius, type) {
+    let url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+    url += `?location=${location}&radius=${radius}&type=${type}&key=${key}`;
+    return await fetch(url, {
+        method: GET,
+        body: null,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+module.exports = {
+    getNearByPlaceSearch
+}
